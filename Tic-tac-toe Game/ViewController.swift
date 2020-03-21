77ce82e08966ee10114cc751fb87d14428564c00
DmitriiOS <dsorlov86@gmail.com>
2020-03-21 15:22:02 +0300

    Initial Commit

---
+++ Tic-tac-toe Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tic-tac-toe Game
+//
+//  Created by 1 on 21.03.2020.
+//  Copyright © 2020 DmitriOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
